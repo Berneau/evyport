ccd05e7de7de3de9945adc004cf2e6b3344d3367
Bernhard Steger <bernhard.steger@gmail.com>
2016-03-19 13:09:37 +0100

    [feature] add lines to sections, add index structure

--- app/wp-content/themes/evyport/index.php
+++ app/wp-content/themes/evyport/index.php
@@ -1,10 +1,36 @@
 <?php get_header(); ?>
 
 
+<!-- Logo -->
+<section>
 
-<h1>Ich bin schön</h1>
+
+  <img src="<?php bloginfo('template_directory');?>/images/line-turquoise.png" class="line layer-1 line-bottom" alt="line" />
+  <img src="<?php bloginfo('template_directory');?>/images/line-light.png" class="line layer-2 line-bottom" alt="line" />
+  <img src="<?php bloginfo('template_directory');?>/images/line-dark.png" class="line layer-3 line-bottom" alt="line" />
+</section>
+
+
+<!-- Projects -->
+<section>
+
+  <!-- <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="blog-post">
+  	      <h2 class="blog-post-title"><?php the_title(); ?></h2>
+        </div>
+      <?php endwhile; ?>
+    <?php endif; ?> -->
+
+</section>
 
 
 
+<!-- Info -->
+<section>
+  <img src="<?php bloginfo('template_directory');?>/images/line-turquoise.png" class="line layer-4 line-top" alt="line" />
+  <img src="<?php bloginfo('template_directory');?>/images/line-light.png" class="line layer-5 line-top" alt="line" />
+  <img src="<?php bloginfo('template_directory');?>/images/line-dark.png" class="line layer-6 line-top" alt="line" />
+
+</section>
 
 <?php get_footer(); ?>
