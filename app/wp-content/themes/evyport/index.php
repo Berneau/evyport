c1534c8a2ca683125fa10964614cc73a073e11f8
Bernhard Steger <bernhard.steger@gmail.com>
2016-03-19 14:03:50 +0100

    [chore] add unaligned logo hexagons

--- app/wp-content/themes/evyport/index.php
+++ app/wp-content/themes/evyport/index.php
@@ -3,7 +3,11 @@
 
 <!-- Logo -->
 <section>
-
+  <div class="logo">
+    <img src="<?php bloginfo('template_directory');?>/images/hexagon-turquoise.png" class="hexagon" alt="hexagon" />
+    <img src="<?php bloginfo('template_directory');?>/images/hexagon-light.png" class="hexagon" alt="hexagon" />
+    <img src="<?php bloginfo('template_directory');?>/images/hexagon-dark.png" class="hexagon" alt="hexagon" />
+  </div>
 
   <img src="<?php bloginfo('template_directory');?>/images/line-turquoise.png" class="line layer-1 line-bottom" alt="line" />
   <img src="<?php bloginfo('template_directory');?>/images/line-light.png" class="line layer-2 line-bottom" alt="line" />
