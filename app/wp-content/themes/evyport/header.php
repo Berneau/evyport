c4011ba86a0d2cc8b70459bd4ff99b84ea5cef6b
Bernhard Steger <bernhard.steger@gmail.com>
2016-03-19 12:24:48 +0100

    [chore] seperate files in header, footer, index

---
+++ app/wp-content/themes/evyport/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
+
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style.css" media="screen" title="no title" charset="utf-8">
+    <!-- <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico"> -->
+    <?php wp_head();?>
+  </head>
+  <body>
---
+++ app/wp-content/themes/evyport/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+
+
+<h1>Ich bin schön</h1>
+
+
+
+
+<?php get_footer(); ?>
